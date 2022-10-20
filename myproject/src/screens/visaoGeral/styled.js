893c109cb54670595bac9eefb772961a6bf3fa3c
juanpablo-git <salesjuan781@gmail.com>
2022-10-19 21:26:56 -0300

    vissao geral

--- myproject/src/screens/visaoGeral/styled.js
+++ myproject/src/screens/visaoGeral/styled.js
@@ -1,10 +1,27 @@
 import styled from "styled-components"
+
+export const T2 = styled.h2`
+
+`
+
+export const ModalContainer = styled.div`
+display: flex;
+background-color: aqua;
+
+`
+
+export const Paragrafo = styled.p`
+font-size:12px ;
+width:50%;
+`
+
 export const Containeer = styled.div`
 background-color: #F5F5F5;
 width: 100vw;
 height: 100vh;
 `
 export const DivTop = styled.div`
+
     width: 100%;
     height: 50%;
 `
