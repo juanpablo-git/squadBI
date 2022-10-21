23ed9f33082c9477d18dca71e7fc3fb03d63fdba
juanpablo-git <salesjuan781@gmail.com>
2022-10-21 20:34:46 -0300

    terminando a visao geral

--- myproject/src/screens/visaoGeral/index.js
+++ myproject/src/screens/visaoGeral/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Containeer, DivButton, DivTop, ModalContainer, Paragrafo, T2, TituloContainer } from "./styled"
+import { Containeer, DivButton, DivTop, Dl, Dt, ListaDireita, ListaEsquerda, ModalContainer, ModalContainerButton, Paragrafo, T2, TituloContainer } from "./styled"
 export default function VisaoGeral(){
     const [opacity,setOpacity] = useState(0)
     useEffect(()=>{
@@ -39,10 +39,44 @@ return(
                     </Paragrafo>
                 </ModalContainer>
             </DivTop>
+
+
+
+
             <DivButton>
                 <TituloContainer opacity={opacity}>
-                  <T2>Visão Geral</T2>
+                <T2 style={{color:"#f2f4f7"}}>Porquê a SquadBI? </T2>
                 </TituloContainer>
+               <ModalContainerButton opacity={opacity}>
+                <ListaEsquerda>
+                <Dl>
+                    <Dt>
+                    <strong>–</strong> Proficionais Qualificados
+                    </Dt>
+                    <Dt>                  
+                    <strong>–</strong> Para todo projeto utilizamos metodologia ágil.
+                    </Dt>
+                    <Dt>                  
+                    <strong>–</strong> Plataforma é simples de usar
+                    </Dt>
+                </Dl>
+                </ListaEsquerda>
+                <ListaDireita>
+                <Dl>
+                    <Dt>
+                    <strong>–</strong> Utilizamos o que há de mais atual em tecnologia para melhorar a performance e qualidade dos seus projetos.
+                    </Dt>
+                    <Dt>                  
+                    <strong>–</strong> Conseguimos unir diversas fontes de dados em uma única ferramenta para gerar o seu Dashboard de maneira eficiente.
+                    Suporte e Treinamento
+                    </Dt>
+                    <Dt>                  
+                    <strong>–</strong>  Garantimos todo suporte e treinamento necessário após a finalização do seu projeto.
+                    </Dt>
+                </Dl>
+                </ListaDireita>
+               </ModalContainerButton>
+               
             </DivButton>
 
         </Containeer>
--- myproject/src/screens/visaoGeral/styled.js
+++ myproject/src/screens/visaoGeral/styled.js
@@ -1,5 +1,17 @@
 import styled from "styled-components"
 import {fadeIn} from "../home/animations"
+
+export const Dl = styled.ul`
+list-style: none;
+`
+export const Dt = styled.li`
+padding-top: 10px;
+font-size: 14px;
+padding-bottom: 10px;
+
+
+`
+
 export const T2 = styled.h2`
 
 `
@@ -13,9 +25,31 @@ opacity: ${(props => props.opacity)};
 animation:3s ${fadeIn} ;
 transition: 2s;
 `
+
+export const ModalContainerButton = styled.div`
+color: #f2f4f7;
+display: flex;
+justify-content: space-between;
+opacity: ${(props => props.opacity)};
+animation:3s ${fadeIn} ;
+transition: 2s;
+`
+
+export const ListaEsquerda = styled.div`
+padding-left: 50px;
+width: 50%;
+`
+export const ListaDireita = styled.div`
+padding-right: 50px;
+width: 50%;
+
+`
+
 export const ModalContainer = styled.div`
 
 display: flex;
+justify-content:center;
+align-items: center;
 opacity: ${(props => props.opacity)};
 animation:3s ${fadeIn} ;
 transition: 2s;
@@ -33,7 +67,7 @@ transition-property: opacity, left, top, height;
 export const Containeer = styled.div`
 background-color: #F5F5F5;
 width: 100vw;
-height: 100vh;
+height: auto;
 `
 export const DivTop = styled.div`
 
@@ -41,7 +75,9 @@ export const DivTop = styled.div`
     height: 50%;
 `
 export const DivButton = styled.div`
-    width: 50%;
-    height: 100vh;
+background: linear-gradient(180deg, #101828 0%, rgba(16, 24, 40, 0.89) 100%);
+padding: 10px;
+    width: 100%;
+    height: 50%;
 
 `
