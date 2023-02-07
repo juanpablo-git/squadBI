ff1fa7ff7dab3beebe6a9792a27653a3270f15a3
juanpablo-git <salesjuan781@gmail.com>
2023-02-07 14:59:59 -0300

    site

--- myproject/src/screens/visaoGeral/index.js
+++ myproject/src/screens/visaoGeral/index.js
@@ -1,66 +1,76 @@
 import { useEffect, useState, useRef } from "react"
 import { Containeer, DivButton, DivTop, Dl, Dt, ListaDireita, ListaEsquerda, ModalContainer, ModalContainerButton, Paragrafo, T2, TituloContainer } from "./styled"
 export default function VisaoGeral({ opacity }) {
+    const containerLogos = useRef()
     return (
         <Containeer  >
             <DivTop>
-            <TituloContainer style={{marginBottom:50}} opacity={opacity.mc0} id="mc">
-                            <T2>Empresas que utilizam  a SQUAD BI</T2>
-                        </TituloContainer>
-                <div style={{display:"flex",width:"80%",backgroundColor:"black"}}>
+                <TituloContainer style={{ marginBottom: 50 }} opacity={opacity.mc0} id="mc">
+                    <T2>Empresas que utilizam  a SQUAD BI</T2>
+                </TituloContainer>
+                <div style={{ display: "flex", width: "80%"}}>
                     <button style={{
-                        height:50,
-                        width:55,
-                        borderRadius:"100%",
-                        border:"none",
-                        padding:10,
-                        border:".1rem solid #e4e7ec"
+                        height: 50,
+                        width: 55,
+                        borderRadius: "100%",
+                        border: "none",
+                        padding: 10,
+                        border: ".1rem solid #e4e7ec"
 
-                    }} onClick={(e)=>{
+                    }} onClick={(e) => {
+                        console.log(containerLogos.current)
+                        containerLogos.current.scrollLeft -= containerLogos.current.offsetLeft
+                        
+                        
 
-                    }}><img src="./angulo-esquerdo.svg" style={{width:"100%",height:"100%"}} /></button>
-                    <div onScroll={(e)=>console.log(e)} onClick={(e)=>{
-                        console.log(e.target)
-                        e.target.scrollLeft += e.target.offsetLeft
-                    }} style={{display:"flex",width:"50%",overflowX:"auto",overflowY:"hidden"}}>
-                {
-                    ["./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png","./Logo-Secundária-Degrade-Escuro.png"]
-                    .map(iten=>{
-                        return(
-                            <ModalContainer opacity={opacity.mc0} >
+                    }}><img src="./angulo-esquerdo.svg" style={{ width: "100%", height: "100%" }} /></button>
+                    <div ref={containerLogos} style={{ display: "flex",padding:10,  overflowX: "hidden", overflowY: "hidden",scrollBehavior:"smooth" }}>
+                        {
+                            ["./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png", "./Logo-Secundária-Degrade-Escuro.png"]
+                                .map((iten,k) => {
+                                    return (
+                                        <ModalContainer opacity={opacity.mc0} >
 
-                            <div style={{ minWidth: "auto", width: "500px",padding:10 }} >
-                            <img src="./Logo-Secundária-Degrade-Escuro.png"  style={{width:"100%",height:"100%"}}/>
-                            </div>
-                            </ModalContainer>
+                                            <div style={{ minWidth: "auto", width: "500px", padding: 10 }} >
+                                               <img src="./Logo-Secundária-Degrade-Escuro.png" style={{ width: "100%", height: "100%" }} />
+                                            </div>
+                                        </ModalContainer>
 
 
 
-                        )
+                                    )
 
-                    })
-                    }
+                                })
+                        }
                     </div>
-                                        <button style={{
-                        height:50,
-                        width:55,
-                        borderRadius:"100%",
-                        border:"none",
-                        padding:10,
-                        border:".1rem solid #e4e7ec"
+                    <button style={{
+                        height: 50,
+                        width: 55,
+                        borderRadius: "100%",
+                        border: "none",
+                        padding: 10,
+                        border: ".1rem solid #e4e7ec"
 
-                    }}><img src="./angulo-direito.svg"  style={{width:"100%",height:"100%"}} /></button>
+                    }}
+                    onClick={(e) => {
+                        console.log(containerLogos.current)
+                        containerLogos.current.scrollLeft += 150
+                        // containerLogos.current.offsetLeft
+                        
+                        
+
+                    }}><img src="./angulo-direito.svg" style={{ width: "100%", height: "100%" }} /></button>
 
 
                 </div>
-                    
-                      
-                    {/* <div style={{minWidth:"auto",maxWidth:500}} >
+
+
+                {/* <div style={{minWidth:"auto",maxWidth:500}} >
                         <img src="./home1.png" style={{width:"100%"}}  />
                     </div> */}
-                   
-                  
-                        {/* <Paragrafo >
+
+
+                {/* <Paragrafo >
                             O Squad Bi é uma plataforma de Bi & Analitics inovadora que pretende levar a cultura de gestão inteligente para empresas.
 
                         </Paragrafo>
@@ -78,12 +88,12 @@ export default function VisaoGeral({ opacity }) {
             <DivButton>
 
 
-              
+
 
                 <ModalContainerButton opacity={opacity.mc1}>
-                <TituloContainer opacity={opacity.mc1}>
-                    <T2 >Beneficios da SQUAD BI</T2>
-                </TituloContainer>
+                    <TituloContainer opacity={opacity.mc1}>
+                        <T2 >Beneficios da SQUAD BI</T2>
+                    </TituloContainer>
 
                     <ListaEsquerda>
                         <Dl>
@@ -107,14 +117,14 @@ export default function VisaoGeral({ opacity }) {
                                 <strong>–</strong>  Garantimos todo suporte e treinamento necessário após a finalização do seu projeto.
                             </Dt>
                         </Dl>
-                      
+
                     </ListaEsquerda>
                     <ListaDireita>
                         {/* <Dl> */}
-                            <img src="./Design team(1).gif" height="300" />
+                        <img src="./Design team(1).gif" height="300" />
                         {/* </Dl> */}
-                        
-                       
+
+
                     </ListaDireita>
                 </ModalContainerButton>
 
